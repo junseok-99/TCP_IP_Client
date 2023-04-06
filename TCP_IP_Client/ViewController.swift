66cbdc38d8e4da1c972a33d01696677eabc4494c
junseoks-99 <angec5645@naver.com>
2023-04-06 22:57:37 +0900

    Initial Commit

---
+++ TCP_IP_Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TCP_IP_Client
+//
+//  Created by 장준석 on 2023/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
